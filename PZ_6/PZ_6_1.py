f6405ec0632c2d4558dac54961df665aed114e11
avanturist12 <geniiyyy@gmail.com>
2023-12-13 09:47:50 +0300

    практические

---
+++ PZ_6/PZ_6_1.py
@@ -0,0 +1,25 @@
+title = []
+
+try:
+	n = int(input("Input N size: "))
+	k = int(input("Input K: "))
+	l = int(input("Input L: "))
+except:
+	print('You must be input integer numbers, not other!')
+	exit()
+
+if 1 < k <= l <= n:
+	result = 0
+
+	for i in range(n):
+		title.append(i)
+
+	title = title[:k] + title[l:]
+
+	for i in title:
+		result += i
+
+	average = result / len(title)
+	print('Average: ', average)
+else:
+	print('Please read a task and retry later')
