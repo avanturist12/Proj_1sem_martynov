9030792fbb2ad98a756162a03dfa191fffdbb744
avanturist12 <geniiyyy@gmail.com>
2023-12-16 10:35:22 +0300

    практические

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -1,22 +1,27 @@
+# Описать функцию Mean(X,Y, AMean, GMean), вычисляющую среднее
+# арифметическое AMean = (X+Y)/2 и среднее геометрическое GMean = y/X Y двух
+# положительных чисел X и Y (X и Y - входные, AMean B GMean - выходные 
+# параметры вещественного типа). С помщью этой функции найти среднее 
+# арифметическое и среднее геометрическое для пар (A, B), (A, C), (A, D), если даны
+# A, B, C, D.
 def mean(x, y):
-	if x > 0 and y > 0:
-		a_mean = float((x + y) / 2)
-		g_mean = float(y / x * y)
+    if x > 0 and y > 0:
+        a_mean: float = float((x + y) / 2)
+        g_mean = float(y / x * y)
 
-		return a_mean, g_mean
-	else:
-		return "You must use only positive numbers to get the result"
+        return a_mean, g_mean
+    else:
+        return "You must use only positive numbers to get the result"
 
 
 try:
-	a = int(input("Input a: "))
-	b = int(input("Input b: "))
-	c = int(input("Input c: "))
-	d = int(input("Input d: "))
+    a = int(input("Input a: "))
+    b = int(input("Input b: "))
+    c = int(input("Input c: "))
+    d = int(input("Input d: "))
 
-	print(mean(a, b))
-	print(mean(a, c))
-	print(mean(a, d))
+    print(mean(a, b))
+    print(mean(a, c))
+    print(mean(a, d))
 except:
-	print('You must be input integer numbers!')
-
+    print('You must be input integer numbers!')
